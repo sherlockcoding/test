e352b81510cfc3409843363dd142220d31fe8572
Sherlock <sherlock.coding@gmail.com>
2020-11-06 00:49:55 +0800

    Add device.cs
    add some region define

---
+++ Src/WpfApp1/WpfApp1/Model/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class Device
+    {
+
+        #region " Fields "
+
+        #endregion
+
+        #region " Properties "
+
+        #endregion
+
+        #region " Constructor "
+
+        #endregion
+
+        #region " Methods "
+
+        #endregion
+    }
+}
