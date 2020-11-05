13141002d053194a43687c347f0bf84331d9473b
Sherlock Lin <sherlock.coding@gmail.com>
2020-11-05 22:40:13 +0800

    Add WPF test

---
+++ Src/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_OnClick(object sender, RoutedEventArgs e)
+        {
+            bool rlt = RunTest();
+            string outStr= "Result is " + rlt;
+            MessageBox.Show(outStr);
+        }
+        public bool RunTest()
+        {
+            bool rlt = false;
+
+            int a = 0;
+            int b = 1;
+
+            int c = SumXY(b, a);
+
+            if (c == 1)
+                rlt = true;
+
+            return rlt;
+        }
+        int SumXY(int i, int i1)
+        {
+            int i2 = i + i1;
+            return i2;
+        }
+    }
+}
