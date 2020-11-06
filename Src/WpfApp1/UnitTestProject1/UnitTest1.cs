11d62c9b0e781f9d0c186c9df5180ddd553c81c1
Sherlock <sherlock.coding@gmail.com>
2020-11-06 23:18:28 +0800

    Add the NuitTest packages from NuGet, and add a UnitTest Project

---
+++ Src/WpfApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+
+        [TestMethod]
+        public void Add_positive_interger()
+        {
+            var calculator = new Calculator();
+            int first = 1;
+            int second = 2;
+            var sum = calculator.Add(first, second); 
+            
+            Assert.AreEqual(3, sum);
+
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
